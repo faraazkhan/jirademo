4fb5a08e0995a1f2c72c3326b5e453d7cbb929e3
Faraaz Khan <faraaz@rationalizeit.us>
2012-09-16 18:51:17 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jirademo::Application.config.secret_token = '496b51c6719d665ca65657de741bade65221f9c7045977084a13ee64f7c6767cbe97494dfcd54c3f5a0ca4a4bf0aebb236eec3f429c7d228ce5c921fedec2f31'
